d74fed1ab0467958207c6151e3c8b2ca8d0a57d3
BatanAndrei <yabatan777@gmail.com>
2024-01-26 15:54:54 +0300

    initial commit

--- src/pages/homePageComponent/HomePageComponent.jsx
+++ src/pages/homePageComponent/HomePageComponent.jsx
@@ -6,10 +6,10 @@ const HomePageComponent = () => {
         <>
             <div className={styles.homeContainer}>
                 <div className={styles.blockTitleAboutSite}></div>
-                <h2>ПОЧЕМУ ИМЕННО МЫ</h2>
+                <h2 className={styles.textTitle}>ПОЧЕМУ ИМЕННО МЫ</h2>
                 <div className={styles.blockCarousel}></div>
                 <div className={styles.blockPictureUnderCarousel}></div>
-                <h2>НАШИ ТАРИФЫ</h2>
+                <h2 className={styles.textTitle}>НАШИ ТАРИФЫ</h2>
                 <div className={styles.blockTariffs}></div>
             </div>
         </>
