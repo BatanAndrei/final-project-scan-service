5ed99b4b0e8e9eef1972080e750cec711de504ce
BatanAndrei <yabatan777@gmail.com>
2024-01-26 20:08:34 +0300

    initial commit

--- src/components/thefooter/Thefooter.jsx
+++ src/components/thefooter/Thefooter.jsx
@@ -10,7 +10,10 @@ const Thefooter = () => {
                     <LogoFooter />
                 </div>
                 <div className={styles.contactInfo}>
-                    
+                    <h4 className={styles.contactInfoModify+' '+styles.marginTop}>г. Москва, Цветной б-р, 40</h4>
+                    <h4 className={styles.contactInfoModify}>+7 495 771 21 11</h4>
+                    <h4 className={styles.contactInfoModify}>info@skan.ru</h4>
+                    <h4 className={styles.contactInfoModify+' '+styles.marginTop}>Copyright. 2022</h4>
                 </div>
             </div>
         </footer>
