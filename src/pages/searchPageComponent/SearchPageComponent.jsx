803d9490a657da1974ab3469cd7d6dac2764d937
BatanAndrei <yabatan777@gmail.com>
2024-02-02 14:43:14 +0300

    initial commit

--- src/pages/searchPageComponent/SearchPageComponent.jsx
+++ src/pages/searchPageComponent/SearchPageComponent.jsx
@@ -1,7 +1,15 @@
+import styles from './searchPageComponent.module.css';
+
+
 const SearchPageComponent = () => {
     return (
-        <>
-        </>
+        <div className={styles.containerPage}>
+            <div className={styles.blockSearch}>
+                <div className={styles.titleBlockSearch}></div>
+                <div className={styles.enterSearchData}></div>
+            </div>
+            <div className={styles.blockImages}></div>
+        </div>
     )
 };
 
