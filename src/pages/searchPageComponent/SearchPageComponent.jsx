f064d74fbb11b0f72d1abf558248434b5be2dfce
BatanAndrei <yabatan777@gmail.com>
2024-02-02 15:18:25 +0300

    initial commit

--- src/pages/searchPageComponent/SearchPageComponent.jsx
+++ src/pages/searchPageComponent/SearchPageComponent.jsx
@@ -1,14 +1,26 @@
 import styles from './searchPageComponent.module.css';
+import documentPng from '../../Images/documentPng.png';
+import foldersPng from '../../Images/foldersPng.png';
+import manRocketPng from '../../Images/manRocketPng.png';
 
 
 const SearchPageComponent = () => {
     return (
         <div className={styles.containerPage}>
             <div className={styles.blockSearch}>
-                <div className={styles.titleBlockSearch}></div>
-                <div className={styles.enterSearchData}></div>
+                <div className={styles.titleBlockSearch}>
+                    <h1 className={styles.textTitle}>Найдите необходимые<br/> данные в пару кликов.</h1>
+                    <h2 className={styles.textSubTitle}>Задайте параметры поиска.<br/> Чем больше заполните, тем точнее поиск</h2>
+                </div>
+                <div className={styles.areaEnterSearchData}>
+                    <div className={styles.fieldsDataSearch}></div>
+                </div>
             </div>
-            <div className={styles.blockImages}></div>
+            <div className={styles.blockImages}>
+                <img className={styles.documentImg} src={documentPng}></img>
+                <img className={styles.foldersImg} src={foldersPng}></img>
+                <img className={styles.manRocketImg} src={manRocketPng}></img>
+            </div>
         </div>
     )
 };
