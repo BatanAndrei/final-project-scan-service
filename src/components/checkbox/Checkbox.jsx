53182a8419838f56315431f1040459eb904115cb
BatanAndrei <yabatan777@gmail.com>
2024-02-02 22:23:47 +0300

    initial commit

---
+++ src/components/checkbox/Checkbox.jsx
@@ -0,0 +1,14 @@
+import styles from './checkbox.module.css';
+
+
+const Checkbox = () => {
+    return (
+        <label className={styles.label}>
+            <input type="checkbox" name="1" className={styles.checkbox}/>
+            <span className={styles.fake}></span>
+            <span>1 пересадка</span>
+        </label>
+    )
+};
+
+export default Checkbox;
--- src/pages/searchPageComponent/SearchPageComponent.jsx
+++ src/pages/searchPageComponent/SearchPageComponent.jsx
@@ -1,10 +1,14 @@
+import React, {useState} from 'react';
 import styles from './searchPageComponent.module.css';
 import documentPng from '../../Images/documentPng.png';
 import foldersPng from '../../Images/foldersPng.png';
 import manRocketPng from '../../Images/manRocketPng.png';
+import Checkbox from '../../components/checkbox/Checkbox';
+
 
 
 const SearchPageComponent = () => {
+    const [value, setValue] = useState(null);
     return (
         <div className={styles.containerPage}>
             <div className={styles.blockSearch}>
@@ -15,17 +19,23 @@ const SearchPageComponent = () => {
                 <form className={styles.formFillData}>
                     <div className={styles.inputSelectFilds}>
                         <h2 className={styles.lableText}>ИНН компании *</h2>
-                        <input className={styles.inputINN} placeholder='10 цифр'/>
+                        <input className={styles.input} placeholder='10 цифр'/>
                         <h2 className={styles.lableText}>Тональность</h2>
                         <select name='tonality' className={styles.select}>
                             <option value='positive'>Позитианая</option>
                             <option value='negative'>Негативная</option>
                             <option value='any'>Любая</option> 
                         </select>
-                        <div className={styles.dateFields}></div>
+                        <h2 className={styles.lableText}>Количество документов в выдаче *</h2>
+                        <input className={styles.input} placeholder='От 1 до 1000'/>
+                        <h2 className={styles.lableText}>Диапазон поиска *</h2>
+                        <div className={styles.dateFields}>
+                            <input className={styles.inputDate} defaultValue={new Date()} type="date"/>
+                            <input className={styles.inputDate} type="date"/>
+                        </div>
                     </div>
                     <div className={styles.checkboxButtonSearch}>
-
+                        <Checkbox />
                     </div>
                 </form>
             </div>
