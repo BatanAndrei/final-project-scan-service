adce4a1cf36ff0984c0272fd910134bb829b9723
BatanAndrei <yabatan777@gmail.com>
2024-02-05 12:43:46 +0300

    initial commit

--- src/components/checkbox/Checkbox.jsx
+++ src/components/checkbox/Checkbox.jsx
@@ -6,7 +6,7 @@ const Checkbox = () => {
         <label className={styles.label}>
             <input type="checkbox" name="1" className={styles.checkbox}/>
             <span className={styles.fake}></span>
-            <span>1 пересадка</span>
+            <span>1 пересадка</span>            
         </label>
     )
 };
