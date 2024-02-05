c10d6984d2ff33dd2d7b5873cfdd4832badfd965
BatanAndrei <yabatan777@gmail.com>
2024-02-05 16:11:32 +0300

    initial commit

---
+++ src/components/displyedResultSearch/DisplyedResultSearch.jsx
@@ -0,0 +1,11 @@
+import styles from './displyedResultSearch.module.css';
+
+
+const DisplyedResultSearch = () => {
+    return (
+        <>
+        </>
+    )
+};
+
+export default DisplyedResultSearch;
