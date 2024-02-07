4ae519f1187df36c6a43de58f4da041aef5cac36
BatanAndrei <yabatan777@gmail.com>
2024-02-07 21:17:34 +0300

    initial commit

--- src/components/displyedResultSearch/DisplyedResultSearch.jsx
+++ src/components/displyedResultSearch/DisplyedResultSearch.jsx
@@ -18,7 +18,7 @@ const DisplyedResultSearch = () => {
                 <h2 className={styles.textModifyGeneralSummary}>Общая сводка</h2>
                 <h3 className={styles.textModifyOptionsFound}>Найдено 4 221 вариантов</h3>
 
-                {/* <div className={styles.sliderBeforeLoad}>   //отображение сладера до загрузки данных
+                {/* <div className={styles.sliderBeforeLoad}>   
                     <div className={styles.arrowPrevDecoration}></div>
                     <div className={styles.blockTitleDataSlider}>
                         <h3 className={styles.texModifyTitleSlider}>Период</h3>
@@ -32,7 +32,7 @@ const DisplyedResultSearch = () => {
                     <div className={styles.arrowNextDecoration}></div>
                 </div> */}
 
-                <div className={styles.sliderAfterLoad}>           //отображение сладера после загрузки данных
+                <div className={styles.sliderAfterLoad}>           
                     <div className={styles.blockTitleDataSlider}>
                         <h3 className={styles.texModifyTitleSlider}>Период</h3>
                         <h3 className={styles.texModifyTitleSlider}>Всего</h3>
